3042bb3df5eaa1934d656c2aec1cc4af6625f5ec
denvermullets <devmullets@gmail.com>
2021-02-19 13:29:01 -0500

    :sparkle: load images from AWS S3, start process of randomly selecting team

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
-const PlayerCard = () => {
+const PlayerCard = (props) => {
+  const { playerName } = props
+  const [player, setPlayer] = useState('')
+
+  const findPlayer = (player) => {
+    fetch(`http://localhost:5000/api/v1/players/${player}`)
+      .then((response) => response.json())
+      .then((res) => setPlayer(res))
+  }
+
+  useEffect(() => {
+    findPlayer(playerName)
+  }, [])
+
   return (
     <SingleCard>
-      <img src='https://i.imgur.com/jMsPGtd.jpg' />
+      <img src={player} />
     </SingleCard>
   )
 }
